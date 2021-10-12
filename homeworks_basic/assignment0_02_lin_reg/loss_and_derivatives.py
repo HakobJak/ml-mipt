ae9e9259a98f69f944bcb3bd57ecc912a55ccfbe
Hakob Hakobyan <hakobhakobyan@Hakobs-MacBook-Pro.local>
2021-10-12 16:35:51 +0300

    Done homework

--- homeworks_basic/assignment0_02_lin_reg/loss_and_derivatives.py
+++ homeworks_basic/assignment0_02_lin_reg/loss_and_derivatives.py
@@ -32,8 +32,8 @@ class LossAndDerivatives:
         Comment: If Y is two-dimentional, average the error over both dimentions.
         """
 
-        # YOUR CODE HERE    
-        return 
+        return np.mean(np.sqrt((X.dot(w) - Y)**2))
+         
 
     @staticmethod
     def l2_reg(w):
@@ -46,22 +46,19 @@ class LossAndDerivatives:
         Computes the L2 regularization term for the weight matrix w.
         """
         
-        # YOUR CODE HERE
-        return 
-
-    @staticmethod
+        return np.sum(w.T.dot(w)
+        
+   @staticmethod
     def l1_reg(w):
         """
         w : numpy array of shape (`n_features`, `target_dimentionality`)
-
         Return : float
             single number with sum of the absolute values of the weight matrix ( \sum_{ij} |w_{ij}| )
         
         Computes the L1 regularization term for the weight matrix w.
         """
-
-        # YOUR CODE HERE
-        return 
+        
+        return np.sum(np.abs(w.T, w))
 
     @staticmethod
     def no_reg(w):
@@ -78,7 +75,6 @@ class LossAndDerivatives:
         w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
         
         Return : numpy array of same shape as `w`
-
         Computes the MSE derivative for linear regression (X.dot(w)) with no bias term
         w.r.t. w weight matrix.
         
@@ -86,8 +82,7 @@ class LossAndDerivatives:
         dimension as well, so you need to consider that fact in derivative implementation.
         """
 
-        # YOUR CODE HERE
-        return 
+        return (2 * np.dot(X.T, (X.dot(w) - Y)) ) / (4 * X.shape[0])
 
     @staticmethod
     def mae_derivative(X, Y, w):
@@ -97,7 +92,6 @@ class LossAndDerivatives:
         w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
         
         Return : numpy array of same shape as `w`
-
         Computes the MAE derivative for linear regression (X.dot(w)) with no bias term
         w.r.t. w weight matrix.
         
@@ -105,35 +99,28 @@ class LossAndDerivatives:
         dimension as well, so you need to consider that fact in derivative implementation.
         """
 
-        # YOUR CODE HERE
-        return 
-
+        return (X.T.dot(np.sign(X.dot(w)-Y))) / (4 * X.shape[0])
+    
     @staticmethod
     def l2_reg_derivative(w):
         """
         w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
-
         Return : numpy array of same shape as `w`
-
         Computes the L2 regularization term derivative w.r.t. the weight matrix w.
         """
 
-        # YOUR CODE HERE
-        return 
+        return 2*w
 
     @staticmethod
     def l1_reg_derivative(w):
         """
         Y : numpy array of shape (`n_observations`, `target_dimentionality`) or (`n_observations`,)
         w : numpy array of shape (`n_features`, `target_dimentionality`) or (`n_features`,)
-
         Return : numpy array of same shape as `w`
-
         Computes the L1 regularization term derivative w.r.t. the weight matrix w.
         """
 
-        # YOUR CODE HERE
-        return 
+        return np.zeros_like(w) + 1
 
     @staticmethod
     def no_reg_derivative(w):
